74ce02c1a00327eaa8b4dabb7871fbfc90a74699
Nonthawat K <nontawatk@8interactive.co.th>
2026-06-29 21:34:29 +0700

    Initial Commit

---
+++ Burnrate/BurnrateApp.swift
@@ -0,0 +1,17 @@
+//
+//  BurnrateApp.swift
+//  Burnrate
+//
+//  Created by Nonthawat Kittaywat on 29/6/2569 BE.
+//
+
+import SwiftUI
+
+@main
+struct BurnrateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Burnrate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Burnrate
+//
+//  Created by Nonthawat Kittaywat on 29/6/2569 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
